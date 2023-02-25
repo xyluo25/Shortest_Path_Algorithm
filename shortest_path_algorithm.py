b72f411c1fd2876933a3f4c02293943e30ecca53
xyluo <36498464+Xiangyongluo@users.noreply.github.com>
2023-02-25 15:12:59 -0700

    update learning materials

---
+++ shortest_path_algorithm.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+##############################################################
+# Created Date: Saturday, February 25th 2023
+# Contact Info: luoxiangyong01@gmail.com
+# Author/Copyright: Mr. Xiangyong Luo
+##############################################################
+
+
+class ShortestPathAlgorithm:
+    def __init__(self, graph):
+        self.graph = graph
+        self.path = []
+        self.cost = 0
